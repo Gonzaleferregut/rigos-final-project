b4a0c9538f44883966eb022da00d6e849e1fed55
Rigoberto Gonzaleferregut <rigoberto.gonzaleferregut@gmail.com>
2017-11-09 12:25:27 -0600

    error proofing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { ApolloProvider, createNetworkInterface } from 'react-apollo'
+import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
@@ -14,7 +14,7 @@ import NotFound from './pages/NotFound';
 import './App.css';
 
 const httpLink = new HttpLink({
-  uri: 'https://api.graph.cool/simple/v1/cj9lnrsa36s7t0121n906lvfa'
+  uri: 'https://api.graph.cool/simple/v1/cj9rk264bd1j501136auolac2'
 })
 
 const client = new ApolloClient({
--- src/components/CreateContact.js
+++ src/components/CreateContact.js
@@ -11,167 +11,171 @@ import Sticky4 from './sticky4';
 import '../form.css';
 
 const styles = {
-    errorStyle: {
-        color: red500,
-    },
-    floatingLabelStyle: {
-        color: black800,
-    },
-    floatingLabelFocusStyle: {
-        color: white,
-    },
-    buttonMargin: {
-        margin: 12
-    },
-    dialogButtonMargin: {
-        margin: 5
-    },
-    buttonColor: {
-        color: lightBlue200,
-    }
+  errorStyle: {
+      color: red500,
+  },
+  floatingLabelStyle: {
+      color: black800,
+  },
+  floatingLabelFocusStyle: {
+      color: white,
+  },
+  buttonMargin: {
+      margin: 12
+  },
+  dialogButtonMargin: {
+      margin: 5
+  },
+  buttonColor: {
+      color: lightBlue200,
+  }
 }
 
 const hideAutoFillColorStyle = {
-    WebkitBoxShadow: '0 0 0 1000px skyblue inset'
+  WebkitBoxShadow: '0 0 0 1000px skyblue inset'
 };
 
 class CreateContact extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            fullName: '',
-            email: '',
-            phone: '',
-            interest: '0',
-            open: false
-        }
+  constructor(props) {
+      super(props)
+      this.state = {
+          fullName: '',
+          email: '',
+          phone: '',
+          interest: '0',
+          open: false
+      }
 
-        this.handleTextChange = this.handleTextChange.bind(this)
-        this.handleSelectChange = this.handleSelectChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
+      this.handleTextChange = this.handleTextChange.bind(this)
+      this.handleSelectChange = this.handleSelectChange.bind(this)
+      this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
-    handleTextChange(event) {
-        const name = event.target.name
-        const value = event.target.value
-        this.setState({
-            [name]: value // using ES6 computed property name
-        })
-    }
-
-    handleSelectChange(key) {
+  handleTextChange(event) {
+      const name = event.target.name
+      const value = event.target.value
       this.setState({
-          interest: key.toString()
+          [name]: value // using ES6 computed property name
       })
-    }
+  }
 
-    handleSubmit(event) {
-      event.preventDefault()
-      console.log(this.state)
-      this._createContact()
-    }
+  handleSelectChange(key) {
+    this.setState({
+        interest: key.toString()
+    })
+  }
 
-    _createContact = async () => {
-        const options = ['READING', 'MATH', 'WRITING', 'HOMEWORK']
-        const key = parseInt(this.state.interest)
-        const interest = options[key]
-        const { fullName, email, phone } = this.state
-        try {
-            await this.props.createContactMutation({
-                variables: {
-                    fullName,
-                    email,
-                    phone,
-                    interest
-                }
-            })
-            this.handleOpen()
-        } catch (error) {
-            console.log(error)
-        }
-    }
+  handleSubmit(event) {
+    event.preventDefault()
+    console.log(this.state)
+    this._createContact()
+  }
 
-    state = {
-      open: false,
-    };
+  _createContact = async () => {
+      const options = ['READING', 'MATH', 'WRITING', 'HOMEWORK']
+      const key = parseInt(this.state.interest)
+      const interest = options[key]
+      const { fullName, email, phone } = this.state
+      try {
+          await this.props.createContactMutation({
+              variables: {
+                  fullName,
+                  email,
+                  phone,
+                  interest
+              }
+          })
+          this.handleOpen()
+      } catch (error) {
+          console.log(error)
+      }
+  }
 
-    handleOpen = () => {
-      this.setState({ open: true });
-    };
+  state = {
+    open: false,
+  };
 
-    handleClose = () => {
-      this.setState({ open: false });
-      document.querySelector("form").reset(); // empty out the form after the dialog closes
-    };
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
 
-    render() {
-        const actions = [
-            <RaisedButton
-              label="Close"
-              primary={true}
-              onClick={this.handleClose}
-              style={styles.dialogButtonMargin}
+  handleClose = () => {
+    this.setState({ open: false });
+    document.querySelector("form").reset(); // empty out the form after the dialog closes
+  };
+
+  render() {
+    const actions = [
+      <RaisedButton
+        label="Close"
+        primary={true}
+        onClick={this.handleClose}
+        style={styles.dialogButtonMargin}
+      />
+    ];
+
+    return (
+      <section className="contact-form">
+        <div className="top-panel">
+            <h2>Contact Me</h2>
+        </div>
+        <form autoComplete="on" onSubmit={this.handleSubmit} >
+          <div className="form">
+            <TextField name="fullName" required type="text"floatingLabelText="Full Name:"
+            floatingLabelFixed
+            onChange={this.handleTextChange}
+            floatingLabelStyle={styles.floatingLabelStyle}
+            floatingLabelFocusStyle={styles.floatingLabelFocustyle}
+            inputStyle={hideAutoFillColorStyle}
             />
-        ];
 
-        return (
-            <section className="contact-form">
-                <div className="top-panel">
-                    <h2>Contact Me</h2>
-                </div>
-                <form autoComplete="on" onSubmit={this.handleSubmit} >
-                    <div className="form">
-                        <TextField name="fullName" required type="text" floatingLabelText="Full Name:"
-                            floatingLabelFixed
-                            onChange={this.handleTextChange}
-                            floatingLabelStyle={styles.floatingLabelStyle}
-                            floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
-                            inputStyle={hideAutoFillColorStyle} />
+            <TextField name="email" requiredfloatingLabelText="Email:" type="email"
+            floatingLabelFixed
+            onChange={this.handleTextChange}
+            floatingLabelStyle={styles.floatingLabelStyle}
+            floatingLabelFocusStyle={styles.floatingLabelFocustyle}
+            inputStyle={hideAutoFillColorStyle}
+            />
 
-                        <TextField name="email" required floatingLabelText="Email:" type="email"
-                            floatingLabelFixed
-                            onChange={this.handleTextChange}
-                            floatingLabelStyle={styles.floatingLabelStyle}
-                            floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
-                            inputStyle={hideAutoFillColorStyle} />
+            <TextField name="phone" requiredfloatingLabelText="Phone Number" type="number"
+            floatingLabelFixed
+            onChange={this.handleTextChange}
+            floatingLabelStyle={styles.floatingLabelStyle}
+            floatingLabelFocusStyle={styles.floatingLabelFocustyle}
+            inputStyle={hideAutoFillColorStyle}
+            />
 
-                        <TextField name="phone" required floatingLabelText="Phone Number" type="number"
-                            floatingLabelFixed
-                            onChange={this.handleTextChange}
-                            floatingLabelStyle={styles.floatingLabelStyle}
-                            floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
-                            inputStyle={hideAutoFillColorStyle} />
+            <SelectField onChange={(event,key) =>
+              this.handleSelectChange(key)}
+              floatingLabelText="Interest"
+              floatingLabelStyle={styles.floatingLabelStyle}
+              value={this.state.interest}>
+                <MenuItem value="0" primaryText="Reading" />
+                <MenuItem value="1" primaryText="Math" />
+                <MenuItem value="2" primaryText="Writing" />
+                <MenuItem value="3" primaryText="Homework Help" />
+            </SelectField>
 
-                        <SelectField onChange={(event, key) => this.handleSelectChange(key)}
-                            floatingLabelText="Interest"
-                            floatingLabelStyle={styles.floatingLabelStyle}
-                            value={this.state.interest}>
-                            <MenuItem value="0" primaryText="Reading" />
-                            <MenuItem value="1" primaryText="Math" />
-                            <MenuItem value="2" primaryText="Writing" />
-                            <MenuItem value="3" primaryText="Homework Help" />
-                        </SelectField>
-
-                        <div className="btn">
-                            <RaisedButton label="Submit" type="submit" style={{ ...styles.buttonMargin, ...styles.buttonColor }} />
-                            <RaisedButton label="Reset" type="reset" style={styles.buttonMargin} />
-                        </div>
-                        <Dialog
-                            title="Thank You for you request"
-                            actions={actions}
-                            modal={false}
-                            open={this.state.open}
-                            onRequestClose={this.handleClose}>
-                            I will be sure to reach out to you shortly.
-                        </Dialog>
-                    </div>
-                </form>
-                <div className="info-sticky">
-                    <Sticky4 />
-                </div>
-            </section>
-        )
-    }
+            <div className="btn">
+              <RaisedButton label="Submit" type="submit"style={{ ...styles.buttonMargin,...styles.buttonColor }} />
+              <RaisedButton label="Reset" type="reset"style={styles.buttonMargin} />
+            </div>
+            <Dialog
+              title="Thank You for you request"
+              actions={actions}
+              modal={false}
+              open={this.state.open}
+              onRequestClose={this.handleClose}>
+              I will be sure to reach out to you shortly.
+            </Dialog>
+          </div>
+        </form>
+        <div className="info-sticky">
+          <Sticky4 />
+        </div>
+      </section>
+    )
+  }
 }
 
 const CREATE_CONTACT_MUTATION = gql`
