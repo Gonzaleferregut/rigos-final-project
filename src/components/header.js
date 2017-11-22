ae864d9b7221b28fc93f8aaa66b4b9554950e42e
Rigoberto Gonzaleferregut <rigoberto.gonzaleferregut@gmail.com>
2017-11-22 12:43:05 -0600

    updating ui (menu)

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import FlatButton from 'material-ui/FlatButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import '../styles/header.css'
 
 
@@ -13,14 +17,15 @@ export default class Header extends Component {
             <img className="logo" src="https://www.gravatar.com/userimage/126022818/c60c2cc94305c1a7ad6c8d8d77de3f1d?size=120" alt="Stacked Books" />
             <h1>Elementary Tutor</h1>
             <nav>
-              <ul className="main-menu">
-                <li><FlatButton label="Menu" type="submit" />
-                  <ul className="sub-menu">
-                    <li><Link to='/CreateUser'>Sign up</Link></li>
-                    <li><Link to='/Login'>Log In</Link></li>
-                  </ul>
-                </li>
-              </ul>
+              <IconMenu
+              iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+              anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+              targetOrigin={{horizontal: 'right', vertical: 'bottom'}}>
+                <div className="sub-menu">
+                  <Link to="/CreateUser">Sign Up</Link>
+                  <Link to="/Login">Log In</Link>
+                </div>
+              </IconMenu>
             </nav>
           </div>
         </header>
